dd577edc0aea0e28fa3591f7c48deb69aed47ae2
c4chula <henry.foster71@yahoo.com>
2024-02-21 02:13:11 +0300

    Fixed dateutils function

--- utils/dateutils.py
+++ utils/dateutils.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 from calendar import monthrange
 from typing import Generator
 
+one_day_delta = timedelta(days=1)
 one_week_delta = timedelta(days=7)
 one_month_delta = timedelta(days=30)
 
@@ -21,6 +22,11 @@ def get_current_week_dates(date: datetime) -> Generator[datetime, None, None]:
 
 def get_current_month_dates(date: datetime) -> Generator[datetime, None, None]:
     days_count = monthrange(date.year, date.month)[1]
-    current_date = date
-    for _ in range(1, days_count + 1):
-        yield current_date
+    current_date = datetime(year=date.year, month=date.month, day=1)
+    for _ in range(days_count):
+        yield current_date 
+        current_date += one_day_delta
+
+
+if __name__ == "__main__":
+    print([date for date in get_current_month_dates(get_current_date())])
